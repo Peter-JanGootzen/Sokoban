8b060a5b4a03a4228c4dd8435b386bc67cda549d
Peter-JanGootzen <PeterJan.Gootzen@gmail.com>
2017-09-12 15:08:43 +0200

    Added first models

---
+++ SokobanCLI/Model/Doolhof.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SokobanCLI
+{
+    public class Doolhof
+    {
+        public GameObject FirstGameObject
+        {
+            get => default(GameObject);
+            set
+            {
+            }
+        }
+    }
+}
---
+++ SokobanCLI/Model/GameObject.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SokobanCLI
+{
+    public abstract class GameObject
+    {
+        public GameObject Noord
+        {
+            get => default(GameObject);
+            set
+            {
+            }
+        }
+
+        public GameObject West
+        {
+            get => default(GameObject);
+            set
+            {
+            }
+        }
+
+        public GameObject Oost
+        {
+            get => default(GameObject);
+            set
+            {
+            }
+        }
+
+        public GameObject Zuid
+        {
+            get => default(GameObject);
+            set
+            {
+            }
+        }
+    }
+}
