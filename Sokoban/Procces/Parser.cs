daca0049706cfc34784da492c100fb81fc1e432a
Bram-Boris Meerlo <bbmeerlo@avans.nl>
2017-10-03 15:33:31 +0100

    Parser can now read files and put them in an array

--- Sokoban/Procces/Parser.cs
+++ Sokoban/Procces/Parser.cs
@@ -35,5 +35,48 @@ namespace SokobanCLI
             */
             return null;
         }
+        /*
+        public void printGame()
+        {
+            for (int i = 0; i < horizontalText.Length; i++)
+            {
+                String vertical = "";
+                for (int y = 0; y < horizontalText[i].Length; y++)
+                {
+                    Console.Write(horizontalText[i][y] + "");
+                }
+                Console.WriteLine();
+            }
+        }
+        */
     }
 }
+        {
+            OpenFileDialog fileChooser = new OpenFileDialog();
+            if (fileChooser.ShowDialog() == DialogResult.OK)
+            {
+                String[] horizontalText = File.ReadAllLines(fileChooser.FileName.ToString());
+                int largestSize = 0;
+                for (int i = 0; i < horizontalText.Length; i++)
+                {
+                    if (horizontalText[i].Length > largestSize)
+                    {
+                        largestSize = horizontalText[i].Length;
+                    }
+                }
+
+                String[] verticalText = new String[largestSize];
+                String text = "";
+                for (int i = 0; i < largestSize; i++)
+                {
+                    for (int y = 0; y < horizontalText.Length; y++)
+                    {
+                        if (horizontalText[y].Length > i)
+                        {
+                            text = text + horizontalText[y][i];
+                        }
+                    }
+                    verticalText[i] = text;
+                    text = "";
+                }
+                
--- Sokoban/Procces/Program.cs
+++ Sokoban/Procces/Program.cs
@@ -8,10 +8,12 @@ namespace SokobanCLI
 {
     class Program
     {
+        [STAThread]
         static void Main(string[] args)
         {
             Parser p = new Parser();
             p.LoadLevel();
+            Console.ReadLine();
         }
     }
 }
